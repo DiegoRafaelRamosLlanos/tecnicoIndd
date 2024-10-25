e31967f02b3a8ac4c708d4ee6bd45ace3dd8bec2
ignacio jose rocha <ignac1997nacho@gmail.com>
2024-10-25 01:37:05 -0300

    agrego cambios respomsivos al proyecto

--- proyecto/front/src/dashboard.jsx
+++ proyecto/front/src/dashboard.jsx
@@ -3,77 +3,41 @@ import axios from "axios";
 import jsPDF from "jspdf";
 import { useNavigate } from "react-router-dom"; 
 import './dashboard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Dashboard = ({ onLogout }) => {
   const [datos, setDatos] = useState([]);
   const [editingData, setEditingData] = useState(null); 
+  const [showTable, setShowTable] = useState(false); 
+  const [showList, setShowList] = useState(false); 
   const [formData, setFormData] = useState({
-    marca_temporal: "",
-    DNI: "",
-    apellido: "",
-    nombre: "",
-    localidad: "",
-    tiene_hermanos: "",
-    telefono_alumno: "",
-    apellido_tutor: "",
-    nombre_tutor: "",
-    telefono_tutor: "",
-    telefono_tutor2: "",
-    curso: "",
-    establecimiento_anio_anterior: "",
-    DNI_tutor: "",
-    cuit_tutor: "",
-    enfermedad_cronica: "",
-    cual_enfermedad: "",
-    medicacion: "",
-    cual_medicacion: "",
-    correoElectronico: "",
-    fecha_nacimiento: "",
-    edad: "",
-    lugar_nacimiento: "",
-    nacionalidad: "",
-    domicilio: "",
-    barrio: "",
-    cod_postal: "",
-    materias_adeuda: ""
+    marca_temporal: "", DNI: "", apellido: "", nombre: "", localidad: "", tiene_hermanos: "", telefono_alumno: "",
+    apellido_tutor: "", nombre_tutor: "", telefono_tutor: "", telefono_tutor2: "", curso: "",
+    establecimiento_anio_anterior: "", DNI_tutor: "", cuit_tutor: "", enfermedad_cronica: "", cual_enfermedad: "",
+    medicacion: "", cual_medicacion: "", correoElectronico: "", fecha_nacimiento: "", edad: "", lugar_nacimiento: "",
+    nacionalidad: "", domicilio: "", barrio: "", cod_postal: "", materias_adeuda: ""
   });
 
   const [newData, setNewData] = useState({
-    marca_temporal: "",
-    DNI: "",
-    apellido: "",
-    nombre: "",
-    localidad: "",
-    tiene_hermanos: "",
-    telefono_alumno: "",
-    apellido_tutor: "",
-    nombre_tutor: "",
-    telefono_tutor: "",
-    telefono_tutor2: "",
-    curso: "",
-    establecimiento_anio_anterior: "",
-    DNI_tutor: "",
-    cuit_tutor: "",
-    enfermedad_cronica: "",
-    cual_enfermedad: "",
-    medicacion: "",
-    cual_medicacion: "",
-    correoElectronico: "",
-    fecha_nacimiento: "",
-    edad: "",
-    lugar_nacimiento: "",
-    nacionalidad: "",
-    domicilio: "",
-    barrio: "",
-    cod_postal: "",
-    materias_adeuda: ""
+    marca_temporal: "", DNI: "", apellido: "", nombre: "", localidad: "", tiene_hermanos: "", telefono_alumno: "",
+    apellido_tutor: "", nombre_tutor: "", telefono_tutor: "", telefono_tutor2: "", curso: "",
+    establecimiento_anio_anterior: "", DNI_tutor: "", cuit_tutor: "", enfermedad_cronica: "", cual_enfermedad: "",
+    medicacion: "", cual_medicacion: "", correoElectronico: "", fecha_nacimiento: "", edad: "", lugar_nacimiento: "",
+    nacionalidad: "", domicilio: "", barrio: "", cod_postal: "", materias_adeuda: ""
   });
-  const navigate = useNavigate(); // Hook para redirigir a login
+  
 
-  // Función para cerrar sesión
+  const [filterDNI, setFilterDNI] = useState("");
+  const [filterCurso, setFilterCurso] = useState("");
+  const [filterNombreApellido, setFilterNombreApellido] = useState("");
+  
+  const navigate = useNavigate(); 
+
   const handleLogout = () => {
-    onLogout(); // Llama a la función para actualizar el estado de autenticación en App.jsx
-    navigate("/login"); // Redirigir a la página de login
+    onLogout(); 
+    navigate("/login");
   };
+
   useEffect(() => {
     axios.get("http://localhost:3001/proyecto/datos")
       .then(response => {
@@ -107,13 +71,13 @@ const Dashboard = ({ onLogout }) => {
 
   const handleUpdate = () => {
     axios.put(`http://localhost:3001/proyecto/actualizarUsuario/${editingData.id}`, formData)
-        .then(response => {
-            setDatos(datos.map(dato => (dato.id === editingData.id ? { ...dato, ...formData } : dato)));
-            setEditingData(null); 
-        })
-        .catch(error => {
-            console.error("Error al actualizar el dato:", error);
-        });
+      .then(response => {
+        setDatos(datos.map(dato => (dato.id === editingData.id ? { ...dato, ...formData } : dato)));
+        setEditingData(null); 
+      })
+      .catch(error => {
+        console.error("Error al actualizar el dato:", error);
+      });
   };
 
   const handleNewDataChange = (e) => {
@@ -121,39 +85,21 @@ const Dashboard = ({ onLogout }) => {
     setNewData({ ...newData, [name]: value });
   };
 
+  const handleDNIChange = (e) => {
+    const value = e.target.value.trim(); // Elimina espacios adicionales
+    setFilterDNI(value);
+  };
+
   const handleNewDataSubmit = () => {
     axios.post("http://localhost:3001/proyecto/registrarUsuario", newData)
       .then(response => {
         setDatos([...datos, response.data.data]); 
         setNewData({
-          marca_temporal: "",
-          DNI: "",
-          apellido: "",
-          nombre: "",
-          localidad: "",
-          tiene_hermanos: "",
-          telefono_alumno: "",
-          apellido_tutor: "",
-          nombre_tutor: "",
-          telefono_tutor: "",
-          telefono_tutor2: "",
-          curso: "",
-          establecimiento_anio_anterior: "",
-          DNI_tutor: "",
-          cuit_tutor: "",
-          enfermedad_cronica: "",
-          cual_enfermedad: "",
-          medicacion: "",
-          cual_medicacion: "",
-          correoElectronico: "",
-          fecha_nacimiento: "",
-          edad: "",
-          lugar_nacimiento: "",
-          nacionalidad: "",
-          domicilio: "",
-          barrio: "",
-          cod_postal: "",
-          materias_adeuda: ""
+          marca_temporal: "", DNI: "", apellido: "", nombre: "", localidad: "", tiene_hermanos: "", telefono_alumno: "",
+          apellido_tutor: "", nombre_tutor: "", telefono_tutor: "", telefono_tutor2: "", curso: "",
+          establecimiento_anio_anterior: "", DNI_tutor: "", cuit_tutor: "", enfermedad_cronica: "", cual_enfermedad: "",
+          medicacion: "", cual_medicacion: "", correoElectronico: "", fecha_nacimiento: "", edad: "", lugar_nacimiento: "",
+          nacionalidad: "", domicilio: "", barrio: "", cod_postal: "", materias_adeuda: ""
         });
       })
       .catch(error => {
@@ -175,107 +121,145 @@ const Dashboard = ({ onLogout }) => {
     doc.save("lista_alumnos.pdf");
   };
 
+
+  const filteredData = () => {
+    return datos.filter((dato) => {
+      const matchesDNI = filterDNI ? dato.DNI.toString().includes(filterDNI) : true; // Asegúrate de convertir a string
+      const matchesCurso = filterCurso ? dato.curso.includes(filterCurso) : true;
+      const matchesNombreApellido = filterNombreApellido ? 
+        `${dato.nombre} ${dato.apellido}`.toLowerCase().includes(filterNombreApellido.toLowerCase()) : true;
+      return matchesDNI && matchesCurso && matchesNombreApellido;
+    });
+  };
+  
+
   return (
-    <div>
-      <h1>Dashboard alumnos</h1>
-      <button onClick={handleLogout}>Cerrar sesión</button>
-      <button onClick={exportToPDF}>Exportar a PDF</button>
+    <div className="container-fluid mt-1"> 
+      <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
+      <button className="btn btn-success" onClick={exportToPDF}>Exportar a PDF</button>
 
-      <div>
-        <h2>Agregar Nuevo Dato</h2>
-        {Object.keys(newData).map((key) => {
-          if (["tiene_hermanos", "enfermedad_cronica", "medicacion", "materias_adeuda"].includes(key)) {
-            return (
-              <select
-                key={key}
-                name={key}
-                value={newData[key]}
-                onChange={handleNewDataChange}
-              >
-                <option value="">Seleccione {key.replace(/_/g, " ")}</option>
-                <option value="Sí">Sí</option>
-                <option value="No">No</option>
-              </select>
-            );
-          } else {
-            return (
-              <input
-                key={key}
-                type="text"
-                name={key}
-                value={newData[key]}
-                onChange={handleNewDataChange}
-                placeholder={key.replace(/_/g, " ")}
-              />
-            );
-          }
-        })}
-        <button onClick={handleNewDataSubmit}>Agregar Dato</button>
-      </div>
+      <h4>Agregar Nuevo Dato</h4>
+      <button className="btn btn-info" onClick={() => setShowTable(!showTable)}>
+        {showTable ? "Ocultar Ingreso de Datos" : "Mostrar Ingreso de Datos"}
+      </button>
 
-      <h2>Lista de Datos</h2>
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(formData).map((key) => (
-              <th key={key}>{key.replace(/_/g, " ")}</th>
-            ))}
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {datos.length > 0 ? (
-            datos.map((dato) => (
-              <tr key={dato.id}>
-                {Object.keys(formData).map((key) => (
-                  <td key={key}>{dato[key]}</td>
-                ))}
-                <td>
-                  <button onClick={() => handleEdit(dato)}>Editar</button>
-                  <button onClick={() => handleDelete(dato.id)}>X</button>
-                </td>
+      {showTable && ( 
+        <div>
+          <div className="row g-0">
+          {Object.keys(newData).map((key, index) => (
+    <div className={`col-md-2 p-1`} key={key} style={{ display: "inline-block", width: "11%" }}>
+      {["tiene_hermanos", "enfermedad_cronica", "medicacion", "materias_adeuda"].includes(key) ? (
+        <select className="form-control" name={key} value={newData[key]} onChange={handleNewDataChange}>
+          <option value="">Seleccione {key.replace(/_/g, " ")}</option>
+          <option value="Sí">Sí</option>
+          <option value="No">No</option>
+        </select>
+      ) : (
+        <input
+          type="text"
+          className="form-control"
+          name={key}
+          value={newData[key]}
+          onChange={handleNewDataChange}
+          placeholder={key.replace(/_/g, " ")}
+        />
+      )}
+    </div>
+  ))}
+</div>
+          <button className="btn btn-primary mt-1" onClick={handleNewDataSubmit}>Agregar Dato</button>
+        </div>
+      )}
+
+      <button className="btn btn-info mt-1" onClick={() => setShowList(!showList)}>
+        {showList ? "Ocultar Lista de Datos" : "Mostrar Lista de Datos"}
+      </button>
+
+      {showList && ( 
+        <div>
+          <h2>Lista de Datos</h2>
+          
+          
+          
+<div className="d-flex mb-3">
+<input 
+  type="text" 
+  placeholder="Filtrar por DNI" 
+  className="form-control mx-1" 
+  value={filterDNI} 
+  onChange={handleDNIChange} 
+/>
+  <input 
+    type="text" 
+    placeholder="Filtrar por Curso" 
+    className="form-control mx-1" 
+    value={filterCurso} 
+    onChange={(e) => setFilterCurso(e.target.value)} 
+  />
+  <input 
+    type="text" 
+    placeholder="Filtrar por Nombre y Apellido" 
+    className="form-control mx-1" 
+    value={filterNombreApellido} 
+    onChange={(e) => setFilterNombreApellido(e.target.value)} 
+  />
+</div>
+
+
+          <table className="table table-responsive">
+            <thead>
+              <tr>
+                <th>DNI</th>
+                <th>Apellido</th>
+                <th>Nombre</th>
+                <th>Curso</th>
+                <th>Acciones</th>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={Object.keys(formData).length + 1}>No hay datos disponibles</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {filteredData().map(dato => (
+                <tr key={dato.id}>
+                  <td>{dato.DNI}</td>
+                  <td>{dato.apellido}</td>
+                  <td>{dato.nombre}</td>
+                  <td>{dato.curso}</td>
+                  <td>
+                    <button className="btn btn-danger" onClick={() => handleDelete(dato.id)}>Eliminar</button>
+                    <button className="btn btn-primary" onClick={() => handleEdit(dato)}>Editar</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
 
       {editingData && (
         <div>
           <h2>Editar Dato</h2>
-          {Object.keys(formData).map((key) => {
-            if (["tiene_hermanos", "enfermedad_cronica", "medicacion", "materias_adeuda"].includes(key)) {
-              return (
-                <select
-                  key={key}
-                  name={key}
-                  value={formData[key]}
-                  onChange={handleFormChange}
-                >
-                  <option value="">Seleccione {key.replace(/_/g, " ")}</option>
-                  <option value="Sí">Sí</option>
-                  <option value="No">No</option>
-                </select>
-              );
-            } else {
-              return (
-                <input
-                  key={key}
-                  type="text"
-                  name={key}
-                  value={formData[key]}
-                  onChange={handleFormChange}
-                  placeholder={key.replace(/_/g, " ")}
-                />
-              );
-            }
-          })}
-          <button onClick={handleUpdate}>Guardar Cambios</button>
-          <button onClick={() => setEditingData(null)}>Cancelar</button>
+          <div className="row g-0">
+          {Object.keys(formData).map((key) => (
+    <div className={`col-md-2 p-1`} key={key} style={{ display: "inline-block", width: "11%" }}>
+      {["tiene_hermanos", "enfermedad_cronica", "medicacion", "materias_adeuda"].includes(key) ? (
+        <select className="form-control" name={key} value={formData[key]} onChange={handleFormChange}>
+          <option value="">Seleccione {key.replace(/_/g, " ")}</option>
+          <option value="Sí">Sí</option>
+          <option value="No">No</option>
+        </select>
+      ) : (
+        <input
+          type="text"
+          className="form-control"
+          name={key}
+          value={formData[key]}
+          onChange={handleFormChange}
+          placeholder={key.replace(/_/g, " ")}
+        />
+      )}
+    </div>
+  ))}
+</div>
+          <button className="btn btn-primary mt-2" onClick={handleUpdate}>Guardar Cambios</button>
         </div>
       )}
     </div>
